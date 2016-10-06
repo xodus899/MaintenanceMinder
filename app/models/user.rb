f9de69dfa175869ba26122f5d9ec1414ced4da3d
Christopher Justice <justcd@mail.broward.edu>
2016-10-06 15:14:25 -0400

    css changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_attached_file :avatar, styles: { thumb: "50x50>" }, default_url:  "/images/car.png"
 
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
-  validates_with AttachmentSizeValidator, attributes: :avatar, less_than: 1.megabytes
+  validates_with AttachmentSizeValidator, attributes: :avatar, less_than: 4.megabytes
 
   validates :name,     presence: true
  
